0a784b24d9ef89aa76637401341b69551dfd53ca
Ivan Bratchikov <ivan.bratchikov@tune-it.ru>
2021-02-09 22:24:09 +0300

    made level bigger

--- src/sections/Level/Articles/Articles.tsx
+++ src/sections/Level/Articles/Articles.tsx
@@ -111,14 +111,14 @@ export const Articles = ({ articles_list }) => {
       <Row>
         <GridFixCol lg={{ span: 23 }} md={{ span: 24 }}>
           <Row gutter={[16, 24]}>
-            <Col lg={{ span: 5 }} md={{ span: 4 }} xs={{ span: 0 }}></Col>
-            <Col lg={{ span: 5 }} md={{ span: 0 }} xs={{ span: 0 }}>
+            <Col lg={{ span: 3 }} md={{ span: 4 }} xs={{ span: 0 }}></Col>
+            <Col lg={{ span: 7 }} md={{ span: 0 }} xs={{ span: 0 }}>
               <Title>Concept</Title>
             </Col>
-            <Col lg={{ span: 5 }} md={{ span: 0 }} xs={{ span: 0 }}>
+            <Col lg={{ span: 7 }} md={{ span: 0 }} xs={{ span: 0 }}>
               <Title>Technology</Title>
             </Col>
-            <Col lg={{ span: 5 }} md={{ span: 0 }} xs={{ span: 0 }}>
+            <Col lg={{ span: 7 }} md={{ span: 0 }} xs={{ span: 0 }}>
               <Title>Coin</Title>
             </Col>
           </Row>
@@ -127,7 +127,7 @@ export const Articles = ({ articles_list }) => {
               <>
                 {i % 3 === 0 && (
                   <ColSpanWrapper
-                    lg={{ span: 5 }}
+                    lg={{ span: 3 }}
                     style={{ alignSelf: "center" }}
                     key={`title__${i}`}
                     md={{ span: 24 }}
@@ -177,8 +177,8 @@ const AricleImg = styled.div`
   background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
-  width: 218px;
-  height: 139px;
+  width: 300px;
+  height: 182px;
   display: flex;
   justify-content: center;
   align-items: center;
